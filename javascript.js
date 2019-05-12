8397a2c9ae9a83f4d956f33ef608a738540b7730
Julenne <46162730+Julenne@users.noreply.github.com>
2019-05-12 18:47:25 -0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-const cards = document.querySelectorAll('.cartaUnica');
-
-function flipCard() {
-    this.classList.toggle('flip');
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard));
